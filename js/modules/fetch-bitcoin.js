efca82dee21c24c64090933bcbe87f830486cffa
Abraao4raujo <a68816619@gmail.com>
2023-12-14 12:25:42 -0300

    refatorando bitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,11 +1,16 @@
-export default function initFetchBitcoin() {}
-
-fetch("https://blockchain.info/ticker")
-  .then((Response) => Response.json())
-  .then((bitcoin) => {
-    const btcPreco = document.querySelector(".btc-preco");
-    btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
-  })
-  .catch((error) => {
-    console.log(Error(error));
-  });
+export default class initFetchBitcoin {
+  fetch(elementBitcoin) {
+    fetch("https://blockchain.info/ticker")
+      .then((Response) => Response.json())
+      .then((bitcoin) => {
+        const btcPreco = document.querySelector(elementBitcoin);
+        btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+      })
+      .catch((error) => {
+        console.log(Error(error));
+      });
+  }
+  init() {
+    this.fetch(".btc-preco");
+  }
+}
